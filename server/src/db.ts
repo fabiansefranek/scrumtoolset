d1cc809f83212b3e55cbc8fa060de06dbae85381
jkuka <jkukacka@gmx.at>
2022-12-06 10:19:26 +0100

    feat(Server): Fix DB Scheme

--- server/src/db.ts
+++ server/src/db.ts
@@ -3,34 +3,18 @@ import mysql from "mysql";
 
 
 export function connect(){return mysql.createConnection({
-    host: '127.0.0.1',
-    port: 3306,
+    host: 'db',
     user: 'root',
     password: 'example',
     database: 'scrumtoolset'
 })}
 
-export function setup(con : mysql.Connection) {
-    console.error("XXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+export function setup(con : mysql.Connection) { //Takes a connection and creates the database scheme there
     con.connect();
-    con.query(`CREATE DATABASE scrumtoolset;
-        USE scrumtoolset;
-        CREATE TABLE User (
-        sessionId varchar(255) not null,
-        username varchar(255) not null,
-        createdAt int not null,
-        roomId int,
-        PRIMARY KEY (sessionId),
-        FOREIGN KEY (roomId) REFERENCES Room(id)
-        );
-        CREATE TABLE Room(
-        id varchar(255) primary key,
-        displayName varchar(255) not null,
-        state varchar(255) not null,
-        createdAt int not null
-        );
-        CREATE TABLE Estimation(
-        );`, (err, rows) => {
+    con.query("CREATE TABLE Room( id varchar(255) primary key, displayName varchar(255) not null, state varchar(255) not null, createdAt int not null );", (err, rows) => {
+        if(err) throw err;
+    });
+    con.query("CREATE TABLE User ( sessionId varchar(255) not null, username varchar(255) not null, createdAt int not null, roomId varchar(255), PRIMARY KEY (sessionId), FOREIGN KEY (roomId) REFERENCES Room(id) );", (err, rows) => {
         if(err) throw err;
     });
 }
