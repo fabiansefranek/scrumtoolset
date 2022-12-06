eab16439d49cd578f6f245d2fe28232903422c32
Fabian <50768355+fabiansefranek@users.noreply.github.com>
2022-12-06 13:59:40 +0100

    fix(docker-db): correct timestamp and docker server depends on

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
 function App() {
+  const [socket, setSocket] = useState<any>(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [username, setUsername] = useState<string>("");
+  const [roomname, setRoomname] = useState("");
+
+  useEffect(() => {
+    if(socket === null) return;
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('room:joined', () => {
+      console.log('joinedRoom')
+      setIsConnected(true)
+    })
+
+    socket.on('error', (args : any) => {
+      console.error(JSON.parse(args))
+    })
+
+    return () => {
+      socket.off('room:joined');
+      socket.off('disconnect');
+    };
+  }, [socket]);
+
+  function disconnect() {
+    socket.disconnect();
+  }
+
+  function connect() {
+    const socket = io('http://localhost:3000')
+    setSocket(socket)
+    return socket;
+  }
+
+  function createRoom() {
+    console.log('create room')
+    const userName = "fabian";
+    const socket = connect()
+    socket.emit('room:create',[roomname, username])
+  }
+
   return (
-    <>
-    </>
+    <div>
+      <p>Connected: { '' + isConnected }</p>
+      {!isConnected && <input type="text" onInput={(event : any) => setUsername(event.target.value)}></input>}
+      {!isConnected && <input type="text" onInput={(event : any) => setRoomname(event.target.value)}></input>}
+      {isConnected && <button onClick={ disconnect }>Disconnect</button>}
+      {!isConnected && <button onClick={ createRoom }>Create room</button>}
+    </div>
   );
 }
 
