910a2254c809279b04780adffb3f2c8cbeca626e
jkuka <jkukacka@gmx.at>
2022-12-12 19:21:34 +0100

    feat(Server): Bundled Starting Events

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import {Server, Socket} from "socket.io";
 import * as dotenv from 'dotenv';
 import {connect, setup} from "./db";
-import {join, create, leave, handleVote, setVotingSystem, close, addUserstories} from './room';
+import {join, create, leave, handleVote, close} from './room';
 dotenv.config();
 
 export const connection = connect();
@@ -17,10 +17,8 @@ io.on("connection",  (socket : Socket) => {
     socket.on("room:join", (arg : any) => join(arg, socket));
     socket.on("room:create", (arg : any) => create(arg, socket));
     socket.on("room:vote", (arg : any) => handleVote(arg, socket));
-    socket.on("room:selectVotingSystem", (arg : any) => setVotingSystem(arg, socket));
     socket.on("room:close", (arg : any) => close(arg, socket));
     socket.on("disconnecting", (reason : any) => leave(socket));
-    socket.on("room:addUserstories", (arg : any) => addUserstories(arg, socket))
 });
 
 io.listen(parseInt(process.env.PORT!));
