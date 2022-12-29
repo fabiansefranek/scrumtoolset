b1b2a2fc644e51a4bbcb6c22dbc9e10425cc6d31
Fabian <fabiansefranek@gmail.com>
2022-12-29 11:21:46 +0100

    fix(frontend): move index.css to new styles folder

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/index.css";
 import { ProviderWrapper } from "./providers/ProviderWrapper";
 
 const root = ReactDOM.createRoot(
