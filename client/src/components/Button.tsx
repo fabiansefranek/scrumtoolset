f5aeb3a35567b2d15e10509398099628abdc655d
fabiansefranek <fabiansefranek@gmail.com>
2023-02-23 22:32:27 +0100

    ✨ Implement button secondary style

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -3,13 +3,21 @@ import { Theme } from "../types";
 
 type Props = {
     theme: Theme;
+    secondary?: boolean;
 };
 
-export const Button = styled.button`
+export const Button = styled.button<Props>`
     border: 0;
     outline: 0;
-    background-color: ${(props: Props) => props.theme.colors.highlight};
-    color: ${(props: Props) => props.theme.colors.highlightText};
+    background-color: ${(props: Props) =>
+        props.secondary ? "transparent" : props.theme.colors.highlight};
+    border: ${(props: Props) =>
+            props.secondary ? props.theme.colors.highlight : "none"}
+        2px solid;
+    color: ${(props: Props) =>
+        props.secondary
+            ? props.theme.colors.highlight
+            : props.theme.colors.highlightText};
     padding: 0.75rem;
     border-radius: 0.25rem;
     cursor: pointer;
@@ -17,6 +25,13 @@ export const Button = styled.button`
 
     &:hover {
         background-color: ${(props: Props) =>
-            props.theme.colors.darkerHighlight};
+            props.secondary
+                ? props.theme.colors.highlight
+                : props.theme.colors.darkerHighlight};
+        color: ${(props: Props) => props.theme.colors.highlightText};
+    }
+
+    &:active {
+        transform: scale(0.995);
     }
 `;
